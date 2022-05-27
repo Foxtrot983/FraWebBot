9ba677a231441f7de84559a99431744c10dcdb7b
Foxtrot983 <wladiksan@gmail.com>
2022-05-27 15:56:24 +0300

    added def to reply

--- tgbotFW/main.py
+++ tgbotFW/main.py
@@ -1,7 +1,6 @@
 import logging
 from aiogram import Bot, Dispatcher, executor, types
-from .config import API_TOKEN
-
+from config import API_TOKEN
 
 logging.basicConfig(level=logging.INFO)
 
@@ -12,14 +11,29 @@ dp = Dispatcher(bot)
 @dp.message_handler(commands=['start', 'help'])
 async def send_welcome(message: types.Message):
 
-    await message.reply("Привет!\nЯ бот компании FraWeb, через меня ты можешь оставить заявку на разработку веб-сайта.😊")
+    await message.answer("Привет!\nЯ бот компании FraWeb, через меня ты можешь оставить заявку на разработку веб-сайта. 😊")
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    buttons = ["О нас", "Контакты", "Оставить заявку"]
+    keyboard.add(*buttons)
+    await message.answer("Далее вы можете узнать о нас, посмотреть контактную информацию, оставить заявку", reply_markup=keyboard)
 
 
+@dp.message_handler(lambda message: message.text == 'О нас')
+async def about(message: types.Message):
+    await message.reply('Тут будет информация о нас!!')
+
+@dp.message_handler(lambda message: message.text == 'Контакты')
+async def about(message: types.Message):
+    await message.reply('Тут будут контакты!!')
+
+@dp.message_handler(lambda message: message.text == 'Оставить заявку')
+async def about(message: types.Message):
+    await message.reply('Тут будет последовательность заборов данных пользователя для его заявки')
 
 @dp.message_handler()
 async def echo(message: types.Message):
 
-    await message.answer(message.text)
+    await message.reply("Моя версия 0.1, фунционал еще не допилен 😠😠😠")
 
 
 if __name__ == '__main__':
