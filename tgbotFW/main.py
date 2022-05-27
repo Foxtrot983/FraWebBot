945fc4998a76c651474cbea23177b9040262cd25
Foxtrot983 <wladiksan@gmail.com>
2022-05-27 14:30:09 +0300

    новый файл:    .gitignore 	новый файл:    tgbotFW/main.py

---
+++ tgbotFW/main.py
@@ -0,0 +1,27 @@
+import logging
+from aiogram import Bot, Dispatcher, executor, types
+
+
+API_TOKEN = '5332758011:AAHqK6tHb2gWMcpxEnWhhMhEIAXcnD1LS_0'
+
+logging.basicConfig(level=logging.INFO)
+
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=['start', 'help'])
+async def send_welcome(message: types.Message):
+
+    await message.reply("Привет!\nЯ бот компании FraWeb, через меня ты можешь оставить заявку на разработку веб-сайта.😊")
+
+
+
+@dp.message_handler()
+async def echo(message: types.Message):
+
+    await message.answer(message.text)
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
